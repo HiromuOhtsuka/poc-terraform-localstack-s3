a26aebc94ed45595c1ae6f1eca645d351456dea6
HiromuOhtsuka <h.ohtsuka.yume@gmail.com>
2021-08-05 22:14:13 +0900

    fix

--- backend.tf
+++ backend.tf
@@ -1,16 +1,3 @@
-variable "s3_endpoint" {}
-
-provider "aws" {
-  region                      = "us-east-1"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_force_path_style         = true
-  endpoints {
-    s3 = var.s3_endpoint
-  }
-}
-
 terraform {
   backend "s3" {
     bucket                      = "test"
--- main.tf
+++ main.tf
@@ -4,6 +4,19 @@ provider "null" {
 resource "null_resource" "test" {
 }
 
+variable "s3_endpoint" {}
+
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_force_path_style         = true
+  endpoints {
+    s3 = var.s3_endpoint
+  }
+}
+
 resource "aws_s3_bucket" "test_create_bucket" {
   bucket = "test-create-bucket"
   acl    = "private"
